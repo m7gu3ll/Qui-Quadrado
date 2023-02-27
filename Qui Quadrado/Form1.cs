70883bddbb3119ad3a6c7b986eb784e7576368ef
m7gu3ll <118573949+m7gu3ll@users.noreply.github.com>
2023-02-27 23:46:35 +0000

    Adicionar arquivos de projeto.

---
+++ Qui Quadrado/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qui_Quadrado
+{
+    public partial class Form1 : Form
+    {
+        TesteQuiQuadrado x;
+        public Form1()
+        {
+            InitializeComponent();
+            x = new TesteQuiQuadrado();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
