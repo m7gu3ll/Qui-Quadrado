ec99c7ca0c87421bf5f28ff02008a07bbd376efb
m7gu3ll <118573949+m7gu3ll@users.noreply.github.com>
2023-02-28 00:12:36 +0000

    mais comentarios e utilizei x pra ver se funcionava

--- Qui Quadrado/Form1.cs
+++ Qui Quadrado/Form1.cs
@@ -12,16 +12,18 @@ namespace Qui_Quadrado
 {
     public partial class Form1 : Form
     {
-        TesteQuiQuadrado x;
+        //Objeto "x" vai executar todas as funções da classe "TesteQuiQuadrado"
+        private static TesteQuiQuadrado x;
         public Form1()
         {
             InitializeComponent();
+            //Objeto "x" inicializado
             x = new TesteQuiQuadrado();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            button1.Text = Convert.ToString(x.QuiQuadrado(80, 90));
         }
 
         private void button1_Click(object sender, EventArgs e)
